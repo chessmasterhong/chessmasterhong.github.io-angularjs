f8862755bad08c769d811e1aad5ef15283270e13
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-26 13:12:20 -0400

    Converted URL links on projects view to hook up with projects controller.

--- scripts/controllers/projectsController.js
+++ scripts/controllers/projectsController.js
@@ -7,36 +7,42 @@ mySite.controller('projectsController', function($scope) {
     $scope.projects = [
         {
             title: 'Fire Emblem: Chronicles of the Abyss',
+            thumbnail_style: '',
             url_details: '',
             url_demo: '',
             url_source: 'https://github.com/chessmasterhong/WaterEmblem'
         },
         {
             title: 'Super Mario World: Koopa Krisis',
+            thumbnail_style: '',
             url_details: '',
             url_demo: 'http://drksephy.bitbucket.org',
             url_source: 'https://bitbucket.org/DrkSephy/smw-koopa-krisis'
         },
         {
             title: 'MathFlash',
+            thumbnail_style: '',
             url_details: '',
             url_demo: '',
             url_source: ''
         },
         {
             title: 'ImpactJS Enemy Editor',
+            thumbnail_style: '',
             url_details: '',
             url_demo: '',
             url_source: 'https://bitbucket.org/chessmasterhong/smw-koopa-krisis_enemy-editor'
         },
         {
             title: 'ParallelText',
+            thumbnail_style: '',
             url_details: '',
             url_demo: 'http://parallel-text.herokuapp.com',
             url_source: ''
         },
         {
             title: 'DocShare',
+            thumbnail_style: '',
             url_details: '',
             url_demo: '',
             url_source: ''
