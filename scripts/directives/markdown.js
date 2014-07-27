4674e597a63c0dc91618433bf6dd34d4bb7cbd6b
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-26 22:40:19 -0400

    Added Showdown to Bower. Added markdown directive. Applied markdown to project details view.

---
+++ scripts/directives/markdown.js
@@ -0,0 +1,12 @@
+/* global mySite, Showdown */
+
+'use strict';
+
+mySite.directive('markdown', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element) {
+            element.html(new Showdown.converter().makeHtml(element.text()));
+        }
+    };
+});
