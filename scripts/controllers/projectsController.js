096eff6dce6e1affce09608475f4e009ad1a6899
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-26 20:12:52 -0400

    Minor fix for flash of unstyled content. Added excerpt property to projects controller.

--- scripts/controllers/projectsController.js
+++ scripts/controllers/projectsController.js
@@ -10,42 +10,48 @@ mySite.controller('projectsController', function($scope) {
             thumbnail_style: '',
             url_details: '',
             url_demo: '',
-            url_source: 'https://github.com/chessmasterhong/WaterEmblem'
+            url_source: 'https://github.com/chessmasterhong/WaterEmblem',
+            excerpt: 'WRITEME'
         },
         {
             title: 'Super Mario World: Koopa Krisis',
             thumbnail_style: '',
             url_details: '',
             url_demo: 'http://drksephy.bitbucket.org',
-            url_source: 'https://bitbucket.org/DrkSephy/smw-koopa-krisis'
+            url_source: 'https://bitbucket.org/DrkSephy/smw-koopa-krisis',
+            excerpt: 'WRITEME'
         },
         {
             title: 'MathFlash',
             thumbnail_style: '',
             url_details: '',
             url_demo: '',
-            url_source: ''
+            url_source: '',
+            excerpt: 'WRITEME'
         },
         {
             title: 'ImpactJS Enemy Editor',
             thumbnail_style: '',
             url_details: '',
             url_demo: '',
-            url_source: 'https://bitbucket.org/chessmasterhong/smw-koopa-krisis_enemy-editor'
+            url_source: 'https://bitbucket.org/chessmasterhong/smw-koopa-krisis_enemy-editor',
+            excerpt: 'WRITEME'
         },
         {
             title: 'ParallelText',
             thumbnail_style: '',
             url_details: '',
             url_demo: 'http://parallel-text.herokuapp.com',
-            url_source: ''
+            url_source: '',
+            excerpt: 'WRITEME'
         },
         {
             title: 'DocShare',
             thumbnail_style: '',
             url_details: '',
             url_demo: '',
-            url_source: ''
+            url_source: '',
+            excerpt: 'WRITEME'
         }
     ];
 });
