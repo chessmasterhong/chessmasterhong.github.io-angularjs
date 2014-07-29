0f592b6e6934c025e6fba5e370066523f3df8d6b
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-28 20:39:20 -0400

    Added gulp-imagemin to gulp task. Updated .gitignore.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 'use strict';
 
 var gulp = require('gulp'),
+    imagemin = require('gulp-imagemin'),
     jshint = require('gulp-jshint');
+
+gulp.task('image', function() {
+    return gulp.src('./media_original/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./media/images/'));
+});
 
 gulp.task('lint', function() {
     return gulp.src('./scripts/**/*.js')
