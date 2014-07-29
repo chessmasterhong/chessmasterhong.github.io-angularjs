b331b2c5b220f288e28f1fab4c34504e7c34e6b1
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-28 23:18:41 -0400

    Added RequireJS main config.

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+requirejs.config({
+    baseUrl: 'scripts',
+    paths: {
+        angular: '../vendor/angular/angular',
+        uiRouter: '../vendor/angular-ui-router/release/angular-ui-router.min'
+    },
+    shim: {
+        angular: { 'exports' : 'angular' }
+    }
+});
+
+define([
+    'require',
+    'angular',
+    'uiRouter',
+    'app'
+], function(require, angular, router, app) {
+    var $html = angular.element(document.getElementsByTagName('html')[0]);
+    angular.element().ready(function() {
+        angular.resumeBootstrap([app['name']]);
+    });
+});
