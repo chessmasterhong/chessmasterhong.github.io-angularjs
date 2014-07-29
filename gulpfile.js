b0b99b58bcd353c644f3ffa1fdc312d09a4dd2bb
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-29 00:53:16 -0400

    Added RequireJS optimizer to gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,35 @@
 var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),
     jshint = require('gulp-jshint'),
-    webserver = require('gulp-webserver');;
-
-gulp.task('image', function() {
-    return gulp.src('./media_original/images/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./media/images/'));
-});
+    requirejs = require('requirejs'),
+    webserver = require('gulp-webserver');
 
 gulp.task('lint', function() {
     return gulp.src('./scripts/**/*.js')
         .pipe(jshint('.jshintrc'))
         .pipe(jshint.reporter('default'));
+});
+
+gulp.task('requirejs', ['lint'], function() {
+    requirejs.optimize({
+        baseUrl: './scripts/',
+        out: './site.js',
+        mainConfigFile: './scripts/main.js',
+        include: ['main'],
+        optimize: 'uglify2',
+        preserveLicenseComments: false
+    }, function() {
+        return 0;
+    }, function(err) {
+        console.log(err);
+        return 1;
+    });
+});
+
+gulp.task('image', function() {
+    return gulp.src('./media_original/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./media/images/'));
 });
 
 gulp.task('webserver', function() {
