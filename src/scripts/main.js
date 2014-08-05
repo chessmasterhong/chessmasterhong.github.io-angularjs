fc9471e262b9107a6d030746a7e5ec14998d60d2
Kevin Chan <chessmasterhong@yahoo.com>
2014-08-04 21:00:36 -0400

    Added angulartics to Bower. Hooked angulartics to RequireJS.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,13 +4,17 @@ requirejs.config({
     baseUrl: './scripts/',
     paths: {
         angular: '../vendor/angular/angular.min',
+        angulartics: '../vendor/angulartics/dist/angulartics.min',
+        angulartics_ga: '../vendor/angulartics/dist/angulartics-ga.min',
         domReady: '../vendor/requirejs-domready/domReady.min',
         ngAnimate: '../vendor/angular-animate/angular-animate.min',
         ngDialog: '../vendor/ngDialog/js/ngDialog.min',
         uiRouter: '../vendor/angular-ui-router/release/angular-ui-router.min'
     },
     shim: {
-        angular: { 'exports': 'angular' },
+        angular: { exports: 'angular' },
+        angulartics: { deps: ['angular'], exports: 'angular' },
+        angulartics_ga: { deps: ['angular', 'angulartics'], exports: 'angular' },
         ngAnimate: { deps: ['angular'], exports: 'angular' },
         ngDialog: { deps: ['angular'], exports: 'angular' },
         uiRouter: { deps: ['angular'], exports: 'angular' }
