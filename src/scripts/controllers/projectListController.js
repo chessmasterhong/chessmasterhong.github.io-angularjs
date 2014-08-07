4b0c536892ce28dcf00b33fb3fd1561b2cd38590
Kevin Chan <chessmasterhong@yahoo.com>
2014-08-07 01:05:01 -0400

    Added ngDialog to project list controller.

--- src/scripts/controllers/projectListController.js
+++ src/scripts/controllers/projectListController.js
@@ -3,8 +3,16 @@ define([
 ], function(controllers) {
     'use strict';
 
-    controllers.controller('projectListController', ['$scope', 'projectsFactory', function($scope, projectsFactory) {
+    controllers.controller('projectListController', ['$scope', 'projectsFactory', 'ngDialog', function($scope, projectsFactory, ngDialog) {
         $scope.projects = projectsFactory.getProjects() || [];
         $scope.view = 'thumbnail';
+
+        $scope.openDialog = function(projectIndex) {
+            console.log(projectIndex)
+            ngDialog.open({
+                template: CONFIG.PATH.TMPL + 'dialogBox-projectList.html',
+                scope: $scope
+            });
+        };
     }]);
 });
