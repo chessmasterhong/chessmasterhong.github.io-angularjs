bc4d438c60616f5267f34525f5e6d7512d7ede46
Kevin Chan <chessmasterhong@yahoo.com>
2014-08-09 10:40:29 -0400

    Added excerpt to Impact Atmosphere project. Trimmed/Revised excerpt contents in other projects'.

--- src/scripts/factories/projectsFactory.js
+++ src/scripts/factories/projectsFactory.js
@@ -11,13 +11,13 @@ define([
                 urlDetails: CONFIG.PATH.POST.PROJ + 'impact_atmospheric_system_plugin.html',
                 urlDemo: 'http://chessmasterhong.bitbucket.org/projects/impact-atmosphere/',
                 urlSource: 'https://github.com/chessmasterhong/impact-atmosphere',
-                excerpt: '',
+                excerpt: 'A plugin for the Impact game engine that simulates an atmospheric weather system, day/night cycles and seasonal cycles based on configurable real-world date, time, and geographical coordinates.',
                 slides: [
-                    { image: CONFIG.PATH.IMG + 'ia0.jpg', caption: '' },
-                    { image: CONFIG.PATH.IMG + 'ia1.jpg', caption: '' },
-                    { image: CONFIG.PATH.IMG + 'ia2.jpg', caption: '' },
-                    { image: CONFIG.PATH.IMG + 'ia3.jpg', caption: '' },
-                    { image: CONFIG.PATH.IMG + 'ia4.jpg', caption: '' }
+                    { image: CONFIG.PATH.IMG + 'ia0.jpg', caption: 'It\'s a rainy day.' },
+                    { image: CONFIG.PATH.IMG + 'ia1.jpg', caption: 'Sunset.' },
+                    { image: CONFIG.PATH.IMG + 'ia2.jpg', caption: 'Nighttime.' },
+                    { image: CONFIG.PATH.IMG + 'ia3.jpg', caption: 'Fog effect using Perlin Noise.' },
+                    { image: CONFIG.PATH.IMG + 'ia4.jpg', caption: 'Let it snow!' }
                 ]
             },
             {
@@ -26,7 +26,7 @@ define([
                 urlDetails: CONFIG.PATH.POST.PROJ + 'fire_emblem_chronicles_of_the_abyss.html',
                 urlDemo: '',
                 urlSource: 'https://github.com/chessmasterhong/WaterEmblem',
-                excerpt: 'Fire Emblem: Chronicles of the Abyss is a web-based remake of Fire Emblem: Blazing Sword, a turn-based strategy role playing game for the Game Boy Advance, but with a different main character and plot twist. The game was built solely for educational purposes under the City College of New York chapter of the Association for Computing Machinery Game Development Student Interest Group. The game was developed in JavaScript utilizing the HTML5 canvas using the ImpactJS game engine. The main goals of this project are to recreate a game that we enjoyed playing, to create a fully completed, quality game, and to give back to the ImpactJS community by open-sourcing the code so as to assist other developers on how to create similar features in their own game. Furthermore, we wanted to see what ImpactJS was truly capable of and to push the limits of the game engine beyond what we had initially thought was possible.',
+                excerpt: 'Fire Emblem: Chronicles of the Abyss is a web-based remake of Fire Emblem: Blazing Sword, a turn-based strategy role playing game for the Game Boy Advance, but with a different main character and plot twist. Developed in JavaScript utilizing the HTML5 canvas using the ImpactJS game engine. The main goals of this project are to recreate a game that we enjoyed playing, to create a fully completed, quality game, and to give back to the ImpactJS community by open-sourcing the code so as to assist other developers on how to create similar features in their own game. Furthermore, we wanted to see what ImpactJS was truly capable of and to push the limits of the game engine beyond what we had initially thought was possible.',
                 slides: [
                     { image: CONFIG.PATH.IMG + 'fe1.jpg', caption: 'Battle animations between a player unit and enemy unit.' },
                     { image: CONFIG.PATH.IMG + 'fe2.jpg', caption: 'Player unit preparing to attack an enemy unit.' }
@@ -38,7 +38,7 @@ define([
                 urlDetails: CONFIG.PATH.POST.PROJ + 'super_mario_world_koopa_krisis.html',
                 urlDemo: 'http://drksephy.bitbucket.org',
                 urlSource: 'https://bitbucket.org/DrkSephy/smw-koopa-krisis',
-                excerpt: 'Super Mario World: Koopa Krisis is a remake of the original Super Mario World platformer for the Super Nintendo Entertainment System. The game was built using JavaScript on top of the ImpactJS game engine. The project was developed under the City College of New York chapter of the Association for Computing Machinery Game Development Student Interest Group for educational purposes only. The goals for this project were to create a web browser version of one of the Nintendo\'s most popular games, to understand how to use ImpactJS, and to learn JavaScript.',
+                excerpt: 'Super Mario World: Koopa Krisis is a remake of the original Super Mario World platformer for the Super Nintendo Entertainment System. Built using JavaScript on top of the ImpactJS game engine. The goals for this project were to create a web browser version of one of the Nintendo\'s most popular games, to understand how to use ImpactJS, and to learn JavaScript.',
                 slides: [
                     { image: CONFIG.PATH.IMG + 'smw0.jpg', caption: 'Title screen.' }
                 ]
@@ -49,7 +49,7 @@ define([
                 urlDetails: CONFIG.PATH.POST.PROJ + 'mathflash.html',
                 urlDemo: '',
                 urlSource: '',
-                excerpt: 'MathFlash is a web-based system designed to address existing problems regarding how mathematical content is displayed on the Internet. It attempts to remedy some of these problems by use of various methods to improve how content is presented and how to get the necessary information across to the user and retaining their learned material more effectively. This project was both an extension of my long-awaited personal desire to create such a system as well as my one-year undergraduate Computer Science capstone project at the City College of New York. The research topics incorporated into this project include adaptive response time-based sequencing and perceptual learning. MathFlash was built using PHP, Python (and a Python library), MySQL, and JavaScript (along with two JavaScript libraries), as well as utilizing HTML5 and CSS3 technologies.',
+                excerpt: 'MathFlash is a web-based system designed to address existing problems regarding how mathematical content is displayed on the Internet. It attempts to remedy some of these problems by use of various methods to improve how content is presented and how to get the necessary information across to the user and retaining their learned material more effectively. Research topics include adaptive response time-based sequencing and perceptual learning. Awarded the 2014 Computer Science Outstanding Project Implementation Award at the City College of New York. MathFlash was built using PHP, Python, JavaScript, and MySQL, as well as utilizing HTML5 and CSS3 technologies.',
                 slides: [
                     { image: CONFIG.PATH.IMG + 'mf0.jpg', caption: 'Table of contents of topics.' }
                 ]
