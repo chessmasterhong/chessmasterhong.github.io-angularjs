64198de6c1595e8894c9a9e9233e5238e38aecd7
Kevin Chan <chessmasterhong@yahoo.com>
2014-08-10 22:42:58 -0400

    Added license concatenation files to build task.

---
+++ src/tasks/license.js
@@ -0,0 +1,32 @@
+/**
+ *  chessmasterhong.github.io
+ *  Copyright (c) 2014, Kevin Chan
+ *  http://chessmasterhong.github.io
+ *  License: MIT
+ *  See: https://github.com/chessmasterhong/chessmasterhong.github.io for details
+ *
+ *  @license AngularJS v1.2.21
+ *  (c) 2010-2014 Google, Inc. http://angularjs.org
+ *  License: MIT
+ *
+ *  @license RequireJS 2.1.14 Copyright (c) 2010-2014, The Dojo Foundation All Rights Reserved.
+ *  Available via the MIT or new BSD license.
+ *  see: http://github.com/jrburke/requirejs for details
+ *
+ *  @license RequireJS domReady 2.0.1 Copyright (c) 2010-2012, The Dojo Foundation All Rights Reserved.
+ *  Available via the MIT or new BSD license.
+ *  see: http://github.com/requirejs/domReady for details
+ *
+ *  State-based routing for AngularJS
+ *  @version v0.2.10
+ *  @link http://angular-ui.github.com/
+ *  @license MIT License, http://www.opensource.org/licenses/MIT
+ *
+ *  @license Angulartics v0.15.20
+ *  (c) 2013 Luis Farzati http://luisfarzati.github.io/angulartics
+ *  License: MIT
+ *
+ *  ngDialog - easy modals and popup windows
+ *  http://github.com/likeastore/ngDialog
+ *  (c) 2013 MIT License, https://likeastore.com
+ */
