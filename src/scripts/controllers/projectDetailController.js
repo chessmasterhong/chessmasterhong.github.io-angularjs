09b5fdeecb9a4546c88043826ec3ef2cf3855a22
Kevin Chan <chessmasterhong@yahoo.com>
2014-08-10 21:28:08 -0400

    Added engine and scripts properties to package.json. Fixed minor JSHint error.

--- src/scripts/controllers/projectDetailController.js
+++ src/scripts/controllers/projectDetailController.js
@@ -3,7 +3,7 @@ define([
 ], function(controllers) {
     'use strict';
 
-    controllers.controller('projectDetailController', ['$scope', '$state', 'projectsFactory', 'ngDialog', function($scope, $state, projectsFactory, ngDialog) {
+    controllers.controller('projectDetailController', ['$scope', '$state', 'projectsFactory', function($scope, $state, projectsFactory) {
         var project = projectsFactory.getProjects(projectsFactory.getProjectCount() - parseInt($state.params.projectIndex) - 1);
         if(project !== null) {
             $scope.project = project;
