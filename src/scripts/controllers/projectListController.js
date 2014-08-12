b4fe600ff42313910c305490d9adb0ca3824869a
Kevin Chan <chessmasterhong@yahoo.com>
2014-08-11 20:22:33 -0400

    Removed ngDialog from project list controller. Tweaked responsive adjustment for project list.

--- src/scripts/controllers/projectListController.js
+++ src/scripts/controllers/projectListController.js
@@ -3,16 +3,7 @@ define([
 ], function(controllers) {
     'use strict';
 
-    controllers.controller('projectListController', ['$scope', 'projectsFactory', 'ngDialog', function($scope, projectsFactory, ngDialog) {
+    controllers.controller('projectListController', ['$scope', 'projectsFactory', function($scope, projectsFactory) {
         $scope.projects = projectsFactory.getProjects() || [];
-        $scope.view = 'thumbnail';
-
-        $scope.openDialog = function(projectIndex) {
-            $scope.projectIndex = $scope.projects.length - projectIndex - 1;
-            ngDialog.open({
-                template: CONFIG.PATH.TMPL + 'dialogBox-projectList.html',
-                scope: $scope
-            });
-        };
     }]);
 });
