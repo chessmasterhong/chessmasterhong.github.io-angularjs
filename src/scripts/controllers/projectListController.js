381eae100cbca7681ea529cddad7fd0cf7fe637a
Kevin Chan <chessmasterhong@yahoo.com>
2014-08-12 18:48:49 -0400

    Fixed incorrect project indexes in project list URLs.

--- src/scripts/controllers/projectListController.js
+++ src/scripts/controllers/projectListController.js
@@ -5,5 +5,10 @@ define([
 
     controllers.controller('projectListController', ['$scope', 'projectsFactory', function($scope, projectsFactory) {
         $scope.projects = projectsFactory.getProjects() || [];
+
+        var projectCount = projectsFactory.getProjectCount();
+        for(var p = 0; p < projectCount; p++) {
+            $scope.projects[p].projectIndex = projectCount - p - 1;
+        }
     }]);
 });
