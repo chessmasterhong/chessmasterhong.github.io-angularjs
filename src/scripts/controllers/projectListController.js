a7aacc6efd50ac71cba13850efc651ae5399d7e9
Kevin Chan <chessmasterhong@yahoo.com>
2014-08-15 21:03:14 -0400

    Revised projectListController.js to work with new project factory.

--- src/scripts/controllers/projectListController.js
+++ src/scripts/controllers/projectListController.js
@@ -4,19 +4,24 @@ define([
     'use strict';
 
     controllers.controller('projectListController', ['$scope', 'projectsFactory', function($scope, projectsFactory) {
-        $scope.projects = projectsFactory.getProjects() || [];
+        $scope.projects = [];
 
-        var projectCount = projectsFactory.getProjectCount();
-        for(var p = 0; p < projectCount; p++) {
-            $scope.projects[p].projectIndex = projectCount - p - 1;
+        projectsFactory.getProjects()
+            .success(function(response) {
+                $scope.projects = response;
 
-            if(window.location.hostname !== 'chessmasterhong.github.io') {
-                $scope.projects[p].thumbnail = '';
+                var projectCount = $scope.projects.length;
+                for(var p = 0; p < projectCount; p++) {
+                    $scope.projects[p].projectIndex = projectCount - p - 1;
 
-                for(var s = 0; s < $scope.projects[p].slides.length; s++) {
-                    $scope.projects[p].slides[s] = '';
+                    if(window.location.hostname !== 'chessmasterhong.github.io') {
+                        $scope.projects[p].thumbnail = '';
+
+                        for(var s = 0; s < $scope.projects[p].slides.length; s++) {
+                            $scope.projects[p].slides[s] = '';
+                        }
+                    }
                 }
-            }
-        }
+            });
     }]);
 });
