7f6d780b2ed325e48249f4560e5bf07161a86525
Kevin Chan <chessmasterhong@yahoo.com>
2014-08-19 23:56:18 -0400

    Added prototype regexp view insertion.

--- build-html.js
+++ build-html.js
@@ -6,9 +6,12 @@
 var fs = require('fs'),
     path = require('path');
 
-fs.readFile(path.join(__dirname, 'src', 'index.html'), 'utf8', function(err, data) {
+fs.readFile(path.join(__dirname, 'src', 'index.html'), 'utf8', function(err, dataIndex) {
     if(!err) {
-        console.log(data);
+        var index = dataIndex.replace(/(ng-cloak|data-ng-cloak)\s*/gi, '');
+        fs.readFile(path.join(__dirname, 'src', 'partials', 'views', 'about.html'), 'utf8', function(err, dataView) {
+            fs.writeFile(path.join(__dirname, 'about.html'), index.replace(/\sdata-ui-view(>)(?=<\/section>)/g, '$1' + dataView), 'utf8');
+        });
     } else {
         console.log(err);
     }
