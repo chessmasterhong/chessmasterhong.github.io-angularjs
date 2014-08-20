6f398e017066d4149b94c51dd5fd437e444e2347
Kevin Chan <chessmasterhong@yahoo.com>
2014-08-20 18:37:09 -0400

    Added HTML fallback generation to about, contact, and credits pages.

--- build-html.js
+++ build-html.js
@@ -15,19 +15,23 @@ fs.readFile(path.join(__dirname, 'src', 'index.html'), charset, function(err, da
                              .replace(/<!--\s*build:css\s+styles\/site\.min\.css\s*-->(\n.*?)*<!--\s*\/build\s*-->/g, '<link rel=stylesheet href="../styles/site.min.css">')
                              .replace(/<!--\s*build:js\s+scripts\/site\.min\.js\s*-->(\n.*?)*<!--\s*\/build\s*-->\n\s*/g, '');
 
-        fs.readFile(path.join(__dirname, 'src', 'partials', 'views', 'about.html'), charset, function(err, dataView) {
-            var dest = path.join(__dirname, 'about', 'index.html');
+        var pages = ['about', 'contact', 'credits'];
 
-            fs.exists(dest, function(exists) {
-                if(!exists) {
-                    fs.mkdir('about');
-                }
+        pages.forEach(function(page) {
+            fs.readFile(path.join(__dirname, 'src', 'partials', 'views', page + '.html'), charset, function(err, dataView) {
+                var dest = path.join(__dirname, page, 'index.html');
 
-                fs.writeFile(
-                    dest,
-                    index.replace(/\sdata-ui-view(>)(?=<\/section>)/g, '$1' + dataView),
-                    charset
-                );
+                fs.exists(dest, function(exists) {
+                    if(!exists) {
+                        fs.mkdir(page);
+                    }
+
+                    fs.writeFile(
+                        dest,
+                        index.replace(/\sdata-ui-view(>)(?=<\/section>)/g, '$1' + dataView),
+                        charset
+                    );
+                });
             });
         });
     } else {
