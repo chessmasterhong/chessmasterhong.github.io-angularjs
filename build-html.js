61d37df5afb206c35ec90d29f15528fe0331c3eb
Kevin Chan <chessmasterhong@yahoo.com>
2014-08-19 23:31:39 -0400

    Added read file contents.

--- build-html.js
+++ build-html.js
@@ -2,3 +2,14 @@
  *  Custom HTML fallback generator for chessmasterhong.github.io
  *  Author: Kevin Chan
  */
+
+var fs = require('fs'),
+    path = require('path');
+
+fs.readFile(path.join(__dirname, 'src', 'index.html'), 'utf8', function(err, data) {
+    if(!err) {
+        console.log(data);
+    } else {
+        console.log(err);
+    }
+});
