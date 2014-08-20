dc7ec00aaa4eb2ea98fcfcb1cca6bf19bba594bf
Kevin Chan <chessmasterhong@yahoo.com>
2014-08-20 18:50:43 -0400

    Added regexp matching for showcase projects.

--- build-html.js
+++ build-html.js
@@ -8,29 +8,54 @@ var fs = require('fs'),
 
 var charset = 'utf8';
 
+//fs.readFile(path.join(__dirname, 'src', 'index.html'), charset, function(err, dataIndex) {
+//    if(!err) {
+//        var index = dataIndex.replace(/(ng-cloak|data-ng-cloak)\s*/g, '')
+//                             .replace(/<!--\s*build:remove\s*-->(\n.*?)*<!--\s*\/build\s*-->\n\s*/g, '')
+//                             .replace(/<!--\s*build:css\s+styles\/site\.min\.css\s*-->(\n.*?)*<!--\s*\/build\s*-->/g, '<link rel=stylesheet href="../styles/site.min.css">')
+//                             .replace(/<!--\s*build:js\s+scripts\/site\.min\.js\s*-->(\n.*?)*<!--\s*\/build\s*-->\n\s*/g, '');
+//
+//        var pages = ['about', 'contact', 'credits'];
+//
+//        pages.forEach(function(page) {
+//            fs.readFile(path.join(__dirname, 'src', 'partials', 'views', page + '.html'), charset, function(err, dataView) {
+//                var dest = path.join(__dirname, page, 'index.html');
+//
+//                fs.exists(dest, function(exists) {
+//                    if(!exists) {
+//                        fs.mkdir(page);
+//                    }
+//
+//                    fs.writeFile(dest, index.replace(/\sdata-ui-view(>)(?=<\/section>)/g, '$1' + dataView), charset);
+//                });
+//            });
+//        });
+//    } else {
+//        console.log(err);
+//    }
+//});
+
 fs.readFile(path.join(__dirname, 'src', 'index.html'), charset, function(err, dataIndex) {
     if(!err) {
-        var index = dataIndex.replace(/(ng-cloak|data-ng-cloak)\s*/g, '')
-                             .replace(/<!--\s*build:remove\s*-->(\n.*?)*<!--\s*\/build\s*-->\n\s*/g, '')
-                             .replace(/<!--\s*build:css\s+styles\/site\.min\.css\s*-->(\n.*?)*<!--\s*\/build\s*-->/g, '<link rel=stylesheet href="../styles/site.min.css">')
-                             .replace(/<!--\s*build:js\s+scripts\/site\.min\.js\s*-->(\n.*?)*<!--\s*\/build\s*-->\n\s*/g, '');
+        var index = dataIndex.replace(/(ng-cloak|data-ng-cloak)\s*/gi, '')
+                             .replace(/<!--\s*build:remove\s*-->(\n.*?)*<!--\s*\/build\s*-->\n\s*/gi, '')
+                             .replace(/<!--\s*build:css\s+styles\/site\.min\.css\s*-->(\n.*?)*<!--\s*\/build\s*-->/gi, '<link rel=stylesheet href="../styles/site.min.css">')
+                             .replace(/<!--\s*build:js\s+scripts\/site\.min\.js\s*-->(\n.*?)*<!--\s*\/build\s*-->\n\s*/gi, '');
 
-        var pages = ['about', 'contact', 'credits'];
+        fs.readFile(path.join(__dirname, 'src', 'partials', 'views', 'projectList.html'), charset, function(err, dataView) {
+            var view = dataView.replace(/<div\s+class="projects">(\n.*)*(?=\n\n<h2>Other\sworks)/gi, '');
 
-        pages.forEach(function(page) {
-            fs.readFile(path.join(__dirname, 'src', 'partials', 'views', page + '.html'), charset, function(err, dataView) {
-                var dest = path.join(__dirname, page, 'index.html');
+            var dest = path.join(__dirname, 'projects', 'index.html');
 
-                fs.exists(dest, function(exists) {
-                    if(!exists) {
-                        fs.mkdir(page);
+            fs.exists(dest, function(exists) {
+                if(!exists) {
+                    fs.mkdir('projects');
+                }
+
+                fs.writeFile(dest, index.replace(/\sdata-ui-view(>)(?=<\/section>)/g, '$1' + view), charset, function(err) {
+                    if(!err) {
+
                     }
-
-                    fs.writeFile(
-                        dest,
-                        index.replace(/\sdata-ui-view(>)(?=<\/section>)/g, '$1' + dataView),
-                        charset
-                    );
                 });
             });
         });
