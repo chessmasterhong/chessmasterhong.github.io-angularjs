93e92956db03e6c3f07efe4ddb6bef216e5fe3e1
Kevin Chan <chessmasterhong@yahoo.com>
2014-08-20 22:25:32 -0400

    Re-enabled HTML fallback generation to about, contact, and credits pages.

--- build-html.js
+++ build-html.js
@@ -8,32 +8,32 @@ var fs = require('fs'),
 
 var charset = 'utf8';
 
-//fs.readFile(path.join(__dirname, 'src', 'index.html'), charset, function(err, dataIndex) {
-//    if(!err) {
-//        var index = dataIndex.replace(/(ng-cloak|data-ng-cloak)\s*/gi, '')
-//                             .replace(/<!--\s*build:remove\s*-->(\n.*?)*<!--\s*\/build\s*-->\n\s*/g, '')
-//                             .replace(/<!--\s*build:css\s+styles\/site\.min\.css\s*-->(\n.*?)*<!--\s*\/build\s*-->/gi, '<link rel=stylesheet href="../styles/site.min.css">')
-//                             .replace(/<!--\s*build:js\s+scripts\/site\.min\.js\s*-->(\n.*?)*<!--\s*\/build\s*-->\n\s*/gi, '');
-//
-//        var pages = ['about', 'contact', 'credits'];
-//
-//        pages.forEach(function(page) {
-//            fs.readFile(path.join(__dirname, 'src', 'partials', 'views', page + '.html'), charset, function(err, dataView) {
-//                var dest = path.join(__dirname, page, 'index.html');
-//
-//                fs.exists(dest, function(exists) {
-//                    if(!exists) {
-//                        fs.mkdir(page);
-//                    }
-//
-//                    fs.writeFile(dest, index.replace(/\sdata-ui-view(>)(?=<\/section>)/gi, '$1' + dataView), charset);
-//                });
-//            });
-//        });
-//    } else {
-//        console.log(err);
-//    }
-//});
+fs.readFile(path.join(__dirname, 'src', 'index.html'), charset, function(err, dataIndex) {
+    if(!err) {
+        var index = dataIndex.replace(/(ng-cloak|data-ng-cloak)\s*/gi, '')
+                             .replace(/<!--\s*build:remove\s*-->(\n.*?)*<!--\s*\/build\s*-->\n\s*/g, '')
+                             .replace(/<!--\s*build:css\s+styles\/site\.min\.css\s*-->(\n.*?)*<!--\s*\/build\s*-->/gi, '<link rel=stylesheet href="../styles/site.min.css">')
+                             .replace(/<!--\s*build:js\s+scripts\/site\.min\.js\s*-->(\n.*?)*<!--\s*\/build\s*-->\n\s*/gi, '');
+
+        var pages = ['about', 'contact', 'credits'];
+
+        pages.forEach(function(page) {
+            fs.readFile(path.join(__dirname, 'src', 'partials', 'views', page + '.html'), charset, function(err, dataView) {
+                var dest = path.join(__dirname, page, 'index.html');
+
+                fs.exists(dest, function(exists) {
+                    if(!exists) {
+                        fs.mkdir(page);
+                    }
+
+                    fs.writeFile(dest, index.replace(/\sdata-ui-view(>)(?=<\/section>)/gi, '$1' + dataView), charset);
+                });
+            });
+        });
+    } else {
+        console.log(err);
+    }
+});
 
 fs.readFile(path.join(__dirname, 'src', 'index.html'), charset, function(err, dataIndex) {
     if(!err) {
