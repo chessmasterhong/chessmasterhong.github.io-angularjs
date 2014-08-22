0c9cbc22899ccab9348a6fd66fbe4662b0b3a4f1
Kevin Chan <chessmasterhong@yahoo.com>
2014-08-21 23:19:27 -0400

    Added placeholder for resource list HTML fallback.

--- build-html.js
+++ build-html.js
@@ -98,3 +98,18 @@ fs.readFile(path.join(__dirname, 'src', 'index.html'), charset, function(err, da
         console.log(err);
     }
 });
+
+fs.readFile(path.join(__dirname, 'src', 'index.html'), charset, function(err, dataIndex) {
+    if(!err) {
+        var index = dataIndex.replace(/(ng-cloak|data-ng-cloak)\s*/gi, '')
+                             .replace(/<!--\s*build:remove\s*-->(\n.*?)*<!--\s*\/build\s*-->\n\s*/gi, '')
+                             .replace(/<!--\s*build:css\s+styles\/site\.min\.css\s*-->(\n.*?)*<!--\s*\/build\s*-->/gi, '<link rel=stylesheet href="../styles/site.min.css">')
+                             .replace(/<!--\s*build:js\s+scripts\/site\.min\.js\s*-->(\n.*?)*<!--\s*\/build\s*-->\n\s*/gi, '');
+
+        fs.readFile(path.join(__dirname, 'src', 'partials', 'views', 'resourceList.html'), charset, function(err, dataView) {
+            // ...
+        });
+    } else {
+        console.log(err);
+    }
+});
