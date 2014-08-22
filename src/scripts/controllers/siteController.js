0ae1d7ec7ce0f18c24d15f6b6d1168de7e34d5ce
Kevin Chan <chessmasterhong@yahoo.com>
2014-08-22 18:41:25 -0400

    Fixed race condition bug with URL overrides in views.

--- src/scripts/controllers/siteController.js
+++ src/scripts/controllers/siteController.js
@@ -3,15 +3,17 @@ define([
 ], function(controllers) {
     'use strict';
 
-    controllers.controller('siteController', [function() {
-        var siteUrls = document.getElementsByTagName('a'),
-            paths = ['projects', 'resources', 'about', 'contact', 'credits'];
+    controllers.controller('siteController', ['$scope', function($scope) {
+        $scope.$on('$viewContentLoaded', function() {
+            var siteUrls = document.getElementsByTagName('a'),
+                paths = ['projects', 'resources', 'about', 'contact', 'credits'];
 
-        for(var i = 0; i < siteUrls.length; i++) {
-            var path = siteUrls[i].href.match(/.*:\/\/[a-z0-9\-.]+(:[0-9]+)?\/(.*)/)[2];
-            if(paths.indexOf(path) >= 0) {
-                siteUrls[i].href = '/#/' + path;
+            for(var i = 0; i < siteUrls.length; i++) {
+                var path = siteUrls[i].href.match(/.*:\/\/[a-z0-9\-.]+(:[0-9]+)?\/(.*)/)[2];
+                if(paths.indexOf(path) >= 0) {
+                    siteUrls[i].href = '/#/' + path;
+                }
             }
-        }
+        });
     }]);
 });
