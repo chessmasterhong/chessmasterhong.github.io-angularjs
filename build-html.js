7b8a3f617856af784db55c2bbbae83ee12a1e6d8
Kevin Chan <chessmasterhong@yahoo.com>
2014-08-22 22:27:33 -0400

    Updated paths to source files in build-html.js.

--- build-html.js
+++ build-html.js
@@ -18,7 +18,7 @@ fs.readFile(path.join(__dirname, 'src', 'index.html'), charset, function(err, da
         var pages = ['about', 'contact', 'credits'];
 
         pages.forEach(function(page) {
-            fs.readFile(path.join(__dirname, 'src', 'partials', 'views', page + '.html'), charset, function(err, dataView) {
+            fs.readFile(path.join(__dirname, 'src', page, page + '.partial.html'), charset, function(err, dataView) {
                 var dest = path.join(__dirname, page, 'index.html');
 
                 fs.exists(dest, function(exists) {
@@ -42,14 +42,14 @@ fs.readFile(path.join(__dirname, 'src', 'index.html'), charset, function(err, da
                              .replace(/<!--\s*build:css\s+styles\/site\.min\.css\s*-->(\n.*?)*<!--\s*\/build\s*-->/gi, '<link rel=stylesheet href="../styles/site.min.css">')
                              .replace(/<!--\s*build:js\s+scripts\/site\.min\.js\s*-->(\n.*?)*<!--\s*\/build\s*-->\n\s*/gi, '');
 
-        fs.readFile(path.join(__dirname, 'src', 'partials', 'views', 'projectList.html'), charset, function(err, dataView) {
+        fs.readFile(path.join(__dirname, 'src', 'projects', 'projectList.partial.html'), charset, function(err, dataView) {
             var showcaseTemplate = dataView.match(/<div\s+data-ng-repeat="project\s+in\s+projects(.*\n)*(?=<\/div>(\n.*)*<h2>Other\sworks)/gi)[0],
                 otherworksTemplate = dataView.match(/<span\s+data-ng-repeat="project\s+in\s+projects(.*\n)*(?=<\/div>)/gi)[0];
 
             var view = dataView.replace(/(<div\s+data-ng-repeat="project\s+in\s+projects)(.*\n)*(?=<\/div>(\n.*)*<h2>Other\sworks)/gi, '')
                                .replace(/<span\s+data-ng-repeat="project\s+in\s+projects(.*\n)*(?=<\/div>)/gi, '');
 
-            fs.readFile(path.join(__dirname, 'src', 'data', 'projects.json'), charset, function(err, dataJSON) {
+            fs.readFile(path.join(__dirname, 'src', 'projects', 'projects.json'), charset, function(err, dataJSON) {
                 var json = JSON.parse(dataJSON.replace(/\)]}',\n/, '')).reverse();
 
                 var row = 0;
@@ -86,11 +86,7 @@ fs.readFile(path.join(__dirname, 'src', 'index.html'), charset, function(err, da
                         fs.mkdir('projects');
                     }
 
-                    fs.writeFile(dest, index.replace(/\sdata-ui-view(>)(?=<\/section>)/gi, '$1' + view), charset, function(err) {
-                        if(!err) {
-
-                        }
-                    });
+                    fs.writeFile(dest, index.replace(/\sdata-ui-view(>)(?=<\/section>)/gi, '$1' + view), charset);
                 });
             });
         });
@@ -106,7 +102,7 @@ fs.readFile(path.join(__dirname, 'src', 'index.html'), charset, function(err, da
                              .replace(/<!--\s*build:css\s+styles\/site\.min\.css\s*-->(\n.*?)*<!--\s*\/build\s*-->/gi, '<link rel=stylesheet href="../styles/site.min.css">')
                              .replace(/<!--\s*build:js\s+scripts\/site\.min\.js\s*-->(\n.*?)*<!--\s*\/build\s*-->\n\s*/gi, '');
 
-        fs.readFile(path.join(__dirname, 'src', 'partials', 'views', 'resourceList.html'), charset, function(err, dataView) {
+        fs.readFile(path.join(__dirname, 'src', 'resources', 'resourceList.partial.html'), charset, function(err, dataView) {
             // ...
         });
     } else {
