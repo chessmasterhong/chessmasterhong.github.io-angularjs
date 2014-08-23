eb71b2266006a7fa6692f86892ce4b2767b94472
Kevin Chan <chessmasterhong@yahoo.com>
2014-08-22 20:48:13 -0400

    Fixed indentation and missing parseInt radix errors.

--- src/projects/projectDetailController.js
+++ src/projects/projectDetailController.js
@@ -4,15 +4,15 @@ define([
     'use strict';
 
     controllers.controller('projectDetailController', ['$scope', '$state', 'projectsFactory', function($scope, $state, projectsFactory) {
-         $scope.projects = [];
-         $scope.project = {};
-         $scope.project.slides = [{}];
+        $scope.projects = [];
+        $scope.project = {};
+        $scope.project.slides = [{}];
 
-         projectsFactory.getProjects()
+        projectsFactory.getProjects()
             .success(function(response) {
                 $scope.projects = response;
 
-                var project = $scope.projects[$scope.projects.length - parseInt($state.params.projectIndex) - 1];
+                var project = $scope.projects[$scope.projects.length - parseInt($state.params.projectIndex, 10) - 1];
 
                 if(project) {
                     $scope.project = project;
