002a5fd6b198cd0cc4f3b5e64d8863b9a63f3e30
Kevin Chan <chessmasterhong@yahoo.com>
2014-08-23 20:00:18 -0400

    Added analytics to script optimization build task.

--- gulpfile.js
+++ gulpfile.js
@@ -78,8 +78,8 @@ gulp.task('optimize-scripts', function() {
         baseUrl: PATH.SOURCE,
         out: PATH.BUILD + 'common/scripts/site.min.js',
         mainConfigFile: PATH.SOURCE + 'common/scripts/main.js',
-        include: ['./vendor/requirejs/require.min.js', 'common/scripts/main'],
-        insertRequire: ['common/scripts/main'],
+        include: ['./vendor/requirejs/require.min.js', './common/scripts/main', './tasks/analytics'],
+        insertRequire: ['./common/scripts/main'],
         wrap: true,
         optimize: 'uglify2',
         preserveLicenseComments: false
